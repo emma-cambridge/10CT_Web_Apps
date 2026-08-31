bdb134a169438f63de163866c82fab2ea7395b73
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2026-08-31 22:28:20 +1000

    Post.reindex is still not working
    back to square one sort of; I'm still getting the same error message that I was at the start. This time I think it might have something to do with my code in __init__.py, as I've kind of chopped up bits and pieces from different tutorials. Something to look into for next time

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 10CT_Task_2
+
+MASTER EVIL PLAN:
+
+ok so set up a relational thing between comments and recipe id so that each post has a recipe id (or title?) for the recipe it was left on. THEN make it so that each page will filter out the recipe id that doesn't apply to it.
--- app/models.py
+++ app/models.py
@@ -6,6 +6,50 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from app.search import add_to_index, remove_from_index, query_index
+
+class SearchableMixin(object):
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+        if total == 0:
+            return [], 0
+        when = []
+        for i in range(len(ids)):
+            when.append((ids[i], i))
+        query = sa.select(cls).where(cls.id.in_(ids)).order_by(
+            db.case(*when, value=cls.id))
+        return db.session.scalars(query), total
+
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
+        }
+
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None
+
+    @classmethod
+    def reindex(cls):
+        for obj in db.session.scalars(sa.select(cls)):
+            add_to_index(cls.__tablename__, obj)
+
+db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
+
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -44,7 +88,8 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-class Recipe(db.Model):
+class Recipe(SearchableMixin, db.Model):
+    __searchable__ = ['title']
     recipe_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     title: so.Mapped[str] = so.mapped_column(sa.String(100))
     recipe_link: so.Mapped[str] = so.mapped_column(sa.String(750))
